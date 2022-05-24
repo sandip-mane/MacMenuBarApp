cff63ea3d5980678ee18336f6a0e7733ad65377a
Sandip Mane <sandip2490@gmail.com>
2022-05-25 00:31:13 +0530

    Initial Commit

---
+++ MacMenuBarApp/MacMenuBarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacMenuBarAppApp.swift
+//  MacMenuBarApp
+//
+//  Created by Sandip Mane on 25/05/22.
+//
+
+import SwiftUI
+
+@main
+struct MacMenuBarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
